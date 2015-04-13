270ec41f97b4d1e084c0a4d9da38b5eecbc72639
Ian Hansen <ihansen@digitalocean.com>
2015-04-13 12:52:38 +0200

    adds tailer

---
+++ tail/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ianschenck/envflag"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// Oplog an individual document from the oplog.rs collection
+type Oplog struct {
+	Timestamp    bson.MongoTimestamp `bson:"ts"`
+	HistoryID    int64               `bson:"h"`
+	MongoVersion int                 `bson:"v"`
+	Operation    string              `bson:"op"`
+	Namespace    string              `bson:"ns"`
+	Object       bson.M              `bson:"o"`
+	QueryObject  bson.Raw            `bson:"o2"`
+}
+
+var (
+	mongoURL = envflag.String("MONGO_URL", "mongodb://localhost", "mongodb url to connect to")
+)
+
+// LatestOplog returns the most recent oplog from the database
+func latestOplog(sess *mgo.Session) (Oplog, error) {
+	var oplog Oplog
+	err := sess.DB("local").C("oplog.rs").Find(nil).Sort("-$natural").One(&oplog)
+	return oplog, err
+}
+
+func main() {
+	envflag.Parse()
+	sess, err := mgo.Dial(*mongoURL)
+	if err != nil {
+		panic(err)
+	}
+
+	// need last oplog timestamp to make tailing query
+	lo, err := latestOplog(sess)
+	if err != nil {
+		panic(err)
+	}
+
+	iter := sess.DB("local").
+		C("oplog.rs").
+		Find(bson.M{"ts": bson.M{"$gte": lo.Timestamp}}).
+		Sort("$natural").
+		LogReplay().
+		Tail(-1) // tail forever
+
+	var oplog Oplog
+	for iter.Next(&oplog) {
+		fmt.Printf("%+v\n", oplog)
+	}
+	err = iter.Err()
+	if err != nil {
+		panic(err)
+	}
+	err = iter.Close()
+	if err != nil {
+		panic(err)
+	}
+}
